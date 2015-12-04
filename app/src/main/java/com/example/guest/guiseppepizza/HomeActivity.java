fe45a27c7bf935aee1ff14d1a169204d64911820
abdul rahimi <afr114@gmail.com>
2015-12-03 17:34:43 -0800

    add photo taking functionality

--- app/src/main/java/com/example/guest/guiseppepizza/HomeActivity.java
+++ app/src/main/java/com/example/guest/guiseppepizza/HomeActivity.java
@@ -3,7 +3,9 @@ package com.example.guest.guiseppepizza;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,11 +19,14 @@ public class HomeActivity extends AppCompatActivity {
         final TextView phoneText = (TextView) findViewById(R.id.phoneTextView);
         final TextView mapText = (TextView) findViewById(R.id.mapTextView);
         final TextView friendText = (TextView) findViewById(R.id.friendTextView);
+        final TextView photoText = (TextView) findViewById(R.id.photoTextView);
         phoneText.setOnClickListener(phoneListener);
         mapText.setOnClickListener(mapListener);
         friendText.setOnClickListener(friendListener);
-
+        photoText.setOnClickListener(photoListener);
     }
+    public static final String TAG = HomeActivity.class.getSimpleName();
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private View.OnClickListener phoneListener = new View.OnClickListener() {
         @Override
@@ -48,6 +53,17 @@ public class HomeActivity extends AppCompatActivity {
             Intent friendIntent = new Intent(Intent.ACTION_SENDTO, uri);
             friendIntent.putExtra("sms_body", "I love Guiseppe's Pizza! ;)");
             startActivity(friendIntent);
+        }
+    };
+
+
+    private View.OnClickListener photoListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (photoIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(photoIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     };
 }
